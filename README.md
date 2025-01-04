bd926b95c1666426215fa3bb61039ec3d9412b9c
VRS-Z <omendes222@gmail.com>
2025-01-04 20:13:42 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Random-Gerator-of-Legends
---
+++ script.js
@@ -0,0 +1,17 @@
+function tierS() {
+  let perfil = document.getElementById("Legend").value;
+  let tierS = (document.getElementById("Here").innerHTML =
+    perfil + " is tier S!");
+}
+
+function tierA() {
+  let perfil = document.getElementById("Legend").value;
+  let tierA = (document.getElementById("Here").innerHTML =
+    perfil + " is tier A.");
+}
+
+function tierB() {
+  let perfil = document.getElementById("Legend").value;
+  let tierB = (document.getElementById("Here").innerHTML =
+    perfil + " is tier B...");
+}
